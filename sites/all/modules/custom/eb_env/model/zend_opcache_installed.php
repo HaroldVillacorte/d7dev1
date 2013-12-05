834b83732bcc4da92800b31be0c0c6e2d1f7fe78
Laughing Host <admin@laughinghost.com>
2013-12-05 11:15:25 -0700

    working on eb-env.

--- sites/all/modules/custom/eb_env/model/zend_opcache_installed.php
+++ sites/all/modules/custom/eb_env/model/zend_opcache_installed.php
@@ -15,18 +15,44 @@ class zend_opcache_installed extends \eb_env\model\parent_class
     {
         parent::__construct(t('Zend OPCache'));
         
+        // Intialize the result string.
+        $result_string = '';
+        
+        // Initialize the result array.
+        $result_array = array(
+            'Zend OPCache Installed' => array(
+                'recommended' => 'Install Zend OPCache',
+            ),
+        );
+        
         // Set result.
-        if (function_exists('opcache_get_status') && function_exists('(opcache_get_configuration')) {
-            $result_string = t('Zend OPCcache is not enabled.');
+        if (!function_exists('opcache_get_status') && function_exists('opcache_get_configuration')) {
+            $result_array['Zend OPCache Installed']['current'] = t('Zend OPCcache is not enabled.');
         }
         else {
             $opcache_config = opcache_get_configuration();
             $version = $opcache_config['version']['version'];
-            $result_string = t('Zend OPCache version ') . $version . t(' is enbaled.');
+            $result_array['Zend OPCache Installed']['current'] = t('Zend OPCache version ') . $version . t(' is enbaled.');
         }
+        
+        foreach ($result_array as $key => $value) {
+            $result_string =
+                '<tr>' .
+                '<td>' . $key . '</td>' .
+                '<td>' . $value['current'] . '</td>' .
+                '<td>' . $value['recommended'] . '</td>' .
+                '</tr>';
+        }
+        
+        
         //var_dump(opcache_get_status());
-        //var_dump(opcache_get_configuration());
-        $this->set_result($result_string);
+        var_dump(opcache_get_configuration());
+        
+        $text_file = t(file_get_contents($this->text_dir . 'result/zend_opcache_installation.txt'));
+        $result = str_replace('{{result}}', $result_string, $text_file);
+        
+        // Run the result setter.
+        $this->set_result($result);
         
         // Set links.
         $this->set_links(array(
